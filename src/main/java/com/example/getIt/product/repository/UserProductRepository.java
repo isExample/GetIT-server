92df8171ac53eb2da181b1c226d08736b0e20564
Gyulguma <ybg653924@gmail.com>
2022-08-08 00:06:25 +0900

    FIX: LikeAPI

--- src/main/java/com/example/getIt/product/repository/UserProductRepository.java
+++ src/main/java/com/example/getIt/product/repository/UserProductRepository.java
@@ -1,14 +1,19 @@
 package com.example.getIt.product.repository;
 
+import com.example.getIt.product.entity.ProductEntity;
 import com.example.getIt.user.entity.UserEntity;
 import com.example.getIt.product.entity.UserProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
 public interface UserProductRepository extends JpaRepository<UserProductEntity, Long> {
 
     List<UserProductEntity> findAllByUserIdx(UserEntity userIdx);
+    UserProductEntity findAllByUserIdxAndProductIdx(UserEntity userIdx, ProductEntity productIdx);
+    @Transactional
+    void deleteByUserProductIdx(Long userProductIdx);
 }
